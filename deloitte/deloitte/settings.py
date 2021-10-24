73e5127b6ac87f72fa6fe97dc627b644af3cc1f6
Unungentium <45122695+Unungentium@users.noreply.github.com>
2021-10-24 18:29:22 +0200

    some more pushing

--- deloitte/deloitte/settings.py
+++ deloitte/deloitte/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'demo-deloitte-fpauwels.herokuap
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,5 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-WHITENOISE_USE_FINDERS = True
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
