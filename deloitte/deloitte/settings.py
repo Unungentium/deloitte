dd5fc426c4bcfcc7c28fbb8f1e40caf583f5c614
Unungentium <45122695+Unungentium@users.noreply.github.com>
2021-10-24 18:45:24 +0200

    some more pushing

--- deloitte/deloitte/settings.py
+++ deloitte/deloitte/settings.py
@@ -124,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+WHITENOISE_USE_FINDERS = True
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
